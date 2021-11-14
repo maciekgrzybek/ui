c9f09891e422459918af290365e94ad3400c56fa
Maciej Grzybek <maciej.grzybek@karnovgroup.com>
2021-11-14 15:54:49 +0100

    test: move mock details to own file

---
+++ src/test-utils/clientDetails.ts
@@ -0,0 +1,3 @@
+export const clientUrl = 'https://rsnibhkhsbfnncjmwnkj.supabase.co'
+export const clientKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNTIxNDE1MywiZXhwIjoxOTMwNzkwMTUzfQ.OQEbAaTfgDdLCCht251P2JRD3QDnui6nsU8N-tZA_Mc'
